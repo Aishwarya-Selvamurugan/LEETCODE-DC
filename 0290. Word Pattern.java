0ea27ff9765efd461c335bb46dc6837448ba90e0
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2023-01-01 09:56:07 +0530

    Create 0290. Word Pattern.java

---
+++ 0290. Word Pattern.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String str[] = s.split(" ");
+        if(str.length != pattern.length())
+        {
+            return false;
+        }
+        Map<Character,String> map = new HashMap<>();
+        for(int i=0;i<pattern.length();i++)
+        {
+            char a = pattern.charAt(i);
+            if(map.containsKey(a) && !map.get(a).equals(str[i]))
+            {
+                return false;
+            }
+            else if(map.containsKey(a) && map.get(a).equals(str[i]))
+            {
+                continue;
+            }
+            else if(map.containsValue(str[i]))
+            {
+                return false;
+            }
+            else 
+            {
+                map.put(a,str[i]);
+            }
+        }
+        return true;
+    }
+}
